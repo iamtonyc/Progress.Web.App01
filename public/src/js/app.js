a94037c5bbf3c291d88d9c5dbe90ae5264cf4165
Tony <iamtonycheung@gmail.com>
2022-01-18 16:43:27 +0800

    up to lesson 50

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,8 +1,53 @@
-
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(function() {
       console.log('Service worker registered!');
+    })
+    .catch(function(err){
+      console.log(err);
     });
 }
+
+window.addEventListener('beforeinstallprompt', function(event){;
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt= event;
+  return false;
+});
+
+
+var promise =new Promise(function (resolve, reject){
+  setTimeout(function(){
+      //resolve('This is executed once the timer is done');
+      reject ({code: 500, message: 'An erorr occured!'});
+
+      //console.log('This is executed once the timer is done');
+  },3000);
+});
+
+promse.then(function(text ){
+  return text;
+}).then(function(newText){
+  console.log(newText);
+}).catch(function(err){
+  console.log(err.code, err.message);
+});
+
+
+
+
+
+
+promse.then(function(text ){
+  return text;
+}).then(function(newText){
+  console.log(newText);
+}).catch(function(err){
+  console.log(err.code, err.message);
+});
+
+
+
+
+console.log('This is executed right after setTimeout()');
--- public/sw.js
+++ public/sw.js
@@ -7,3 +7,43 @@ self.addEventListener('activate', function(event){
     console.log('[Service Worker] Activating Serice Worker ... ', event);
     return self.clients.claim();
 });
+
+self.addEventListener('fetch', function(event){
+    console.log('[Service Worker] Fetchig somethings ... ', event);
+    event.respondWith(fetch(event.request));
+});
+
+var promise =new Promise(function (resolve, reject){
+    setTimeout(function(){
+        //resolve('This is executed once the timer is done');
+        reject ({code: 500, message: 'An erorr occured!'});
+
+        //console.log('This is executed once the timer is done');
+    },3000);
+});
+
+fetch('https://httpbin.org/ips')
+    .then(function(response){
+        console.log(response);
+        response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+    .catch(function(err){
+    console.log(err.code, err.message);
+});
+
+
+promise.then(function(text ){
+    return text;
+}).then(function(newText){
+    console.log(newText);
+}).catch(function(err){
+    console.log(err.code, err.message);
+});
+
+
+
+
+console.log('This is executed right after setTimeout()');
