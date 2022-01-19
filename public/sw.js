b4f768653abc05914deeda1987094537310064a0
Tony <iamtonycheung@gmail.com>
2022-01-19 11:49:04 +0800

    up to lesson 51

--- public/sw.js
+++ public/sw.js
@@ -1,49 +1,14 @@
-self.addEventListener('install', function(event){
-    console.log('[Service Worker] Instalilng Serice Worker ... ', event);
+
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
 });
 
-
-self.addEventListener('activate', function(event){
-    console.log('[Service Worker] Activating Serice Worker ... ', event);
-    return self.clients.claim();
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event){
-    console.log('[Service Worker] Fetchig somethings ... ', event);
-    event.respondWith(fetch(event.request));
+self.addEventListener('fetch', function(event) {
+  console.log('[Service Worker] Fetching something ....', event);
+  event.respondWith(fetch(event.request));
 });
-
-var promise =new Promise(function (resolve, reject){
-    setTimeout(function(){
-        //resolve('This is executed once the timer is done');
-        reject ({code: 500, message: 'An erorr occured!'});
-
-        //console.log('This is executed once the timer is done');
-    },3000);
-});
-
-fetch('https://httpbin.org/ips')
-    .then(function(response){
-        console.log(response);
-        response.json();
-    })
-    .then(function(data){
-        console.log(data);
-    })
-    .catch(function(err){
-    console.log(err.code, err.message);
-});
-
-
-promise.then(function(text ){
-    return text;
-}).then(function(newText){
-    console.log(newText);
-}).catch(function(err){
-    console.log(err.code, err.message);
-});
-
-
-
-
-console.log('This is executed right after setTimeout()');
